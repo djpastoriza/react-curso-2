ad46bb7145e10017402c1fba69906f06ab4c351e
Diego Pastoriza <dpastoriza@cdt.com.ar>
2019-07-16 19:08:20 -0300

    se usa estilos en react

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
+import './Task.css';
 
 class Task extends Component{
 
+    StyleCompleted(){
+        return{
+            fontSize: '20px',
+            color:this.props.details.done ? 'gray' : 'black',
+            textDecoration:'none'
+        }
+    }
+
     render(){
         const {details} = this.props;
+
         return(
             <div>
-                 <p>
+                 <p style={this.StyleCompleted()}>
                     {details.title} - {details.description} - {details.id}
                     <input type="checkbox" />
-                    <button>x</button>
+                    <button style={btnDelete}>x</button>
                 </p>
             </div>
         )
     }
+}
+
+const btnDelete = {
+    fontSize:'18px',
+    background:'#ea2027',
+    color:'#fff',
+    border:'none',
+    padding:'10px 15px',
+    borderRadius:'50%',
+    cursor:'pointer',
 }
 
 export default Task;
